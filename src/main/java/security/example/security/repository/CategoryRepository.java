d00127fb2bb9c20642d18080d2b571f6d0a87862
cuong.nguyen <cuongnq.hust@gmail.com>
2023-10-15 13:53:58 +0700

    15/10 getAllCategory

--- src/main/java/security/example/security/repository/CategoryRepository.java
+++ src/main/java/security/example/security/repository/CategoryRepository.java
@@ -1,9 +1,23 @@
 package security.example.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import security.example.security.model.CategoryMovie;
+import security.example.security.model.Movie;
+
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryMovie, Long> {
     CategoryMovie findCategoryMovieById(Long id);
+    @Query(value = """
+            select * from categorys ct
+             """
+            , countQuery = """
+            select count(ct.id) from categorys ct
+            where 1=1
+            """
+            , nativeQuery = true)
+    List<CategoryMovie> findListCategory();
 }
